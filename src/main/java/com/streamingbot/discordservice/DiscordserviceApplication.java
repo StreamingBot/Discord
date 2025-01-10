3842cf677462d8a3a0c1b548460d21a24e7b671a
Popodil <popodil@bornready.eu>
2025-01-10 03:44:46 +0100

    Initial commit

---
+++ src/main/java/com/streamingbot/discordservice/DiscordserviceApplication.java
@@ -0,0 +1,13 @@
+package com.streamingbot.discordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streamingbot/discordservice/DiscordserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamingbot.discordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
