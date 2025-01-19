31fb5116c3daf0c7b7bc5b28458b60cab0e0eb00
Yoeri Smits <popodil@bornready.eu>
2025-01-19 23:11:57 +0100

    Update UUIDGeneratorImpl.java

--- src/main/java/com/streamingbot/discordservice/services/impl/UUIDGeneratorImpl.java
+++ src/main/java/com/streamingbot/discordservice/services/impl/UUIDGeneratorImpl.java
@@ -1,9 +1,12 @@
 package com.streamingbot.discordservice.services.impl;
 
+import org.springframework.stereotype.Service;
 import java.util.UUID;
 
+@Service
 public class UUIDGeneratorImpl {
-public UUID Generate() {
-    return UUID.randomUUID();
+
+    public UUID Generate() {
+        return UUID.randomUUID();
     }
 }
